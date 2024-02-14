6e4c497f5d191cc28ba1425835ea923bc15b1dff
vigneshkewk <156401889+vigneshkewk@users.noreply.github.com>
2024-02-14 21:56:19 +0530

    Add files via upload

---
+++ 2d lists.py
@@ -0,0 +1,10 @@
+#2d lists = a list of lists (also called as multidimesional lists
+
+drink = ["coffe","tea","soda"]
+dinner = ["pizza","burger","sandwich"]
+desserts = ["cake","ice cream"]
+food = [drink,dinner,desserts]
+#print(food)
+print(food[0][1])
+print(food[1][2])
+print(food[2][0])
